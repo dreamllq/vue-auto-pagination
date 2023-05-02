9004104988fb592a03bf16d2692e8c425d3f855c
lvliqi <lvliqi_d@163.com>
2023-05-02 15:13:43 +0800

    feat: 修改构建

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "My Awesome Project",
-  description: "A VitePress Site",
+  base:'/lc-vue-auto-pagination/docs/.vitepress/dist/',
+  title: "lc-vue-auto-pagination",
+  description: "lc-vue-auto-pagination",
 })
