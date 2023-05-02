316650f0137fd1c1eb6b1c6958ab903ab0a5d6eb
lvliqi <lvliqi_d@163.com>
2023-05-02 15:21:06 +0800

    feat: 文档

---
+++ docs/.vitepress/theme/not-found.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { withBase, useRouter } from 'vitepress'
+const root = ref('/')
+const router = useRouter()
+onMounted(() => {
+  onMounted(()=>{
+    router.go(withBase('/'))
+  })
+})
+</script>
+
+<style scoped>
+
+</style>
