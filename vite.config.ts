504f890c6051c9cb225116f4a32b126057b56fc0
lvliqi <lvliqi_d@163.com>
2023-05-05 23:06:54 +0800

    feat: 修改声明

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     lib: {
       entry: 'src/index.ts',
       name: 'VueAutoPagination',
-      fileName: 'vue-auto-pagination'
+      fileName: 'index'
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
